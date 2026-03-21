a7f293cfc89147fc39def4da5d455c0416648dfb
Kasperls <kasperls004@gmail.com>
2026-03-21 19:42:17 +0100

    Adding command enum

---
+++ src/command.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum class ClCommand {
+    NONE,
+    QUIT,
+    RFID_SCANNED,
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "userManager.h"
 #include "user.h"
+#include "command.h"
 
 #include <iostream>
 #include <string>
 #include <thread>
 #include <chrono>
+
 
 #ifdef __linux__
     #include <pigpio.h>
@@ -12,6 +14,25 @@
 
 #define INPUT_PIN 18  // change this to whatever pin you use
 #define RESET_PIN 16  // change this to whatever pin you use
+
+std::atomic<bool> run = true;
+std::atomic<bool> pay = false;
+std::atomic<ClCommand> cl_command = ClCommand{0};
+
+
+void inputThread() {
+    std::string input;
+    while (run) {
+        std::getline(std::cin, input);
+        std::cout << input << std::endl;
+        if (input == "q") {
+            cl_command = ClCommand::QUIT;
+        }
+        if (input.size() == 10) {
+            cl_command = ClCommand::RFID_SCANNED;
+        }
+    }
+}
 
 #ifdef __linux__
 
@@ -31,13 +52,14 @@ int main() {
     UserManager user_manager = UserManager{"../res/test_data.csv"};
     user_manager.printUsers();
 
-    bool run = true;
 
     int value = 0;
     bool input_pressed = false;
     bool reset_pressed = false;
 
     // --- --- --- PROGRAM LOOP --- --- ---
+    std::thread input(inputThread);
+
     while (run) {
         if (gpioRead(RESET_PIN) && !reset_pressed) {
             std::cout << "Reset pressed!" << std::endl;
@@ -62,6 +84,23 @@ int main() {
             input_pressed = false;
 
         }
+
+        switch () {
+            case ClCommand::NONE:
+                break;
+
+            case ClCommand::QUIT:
+                run = false;
+                cl_command = ClCommand::NONE;
+                break;
+            
+            case ClCommand::RFID_SCANNED:
+                std::cout << "Card scanned!" << std::endl;
+                cl_command = ClCommand::NONE;
+                break;
+        } 
+
+
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
     
