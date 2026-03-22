ca63e3dfcc31fa57b0d56583c4c108239b6fa805
Kasperls <kasperls004@gmail.com>
2026-03-22 22:53:12 +0100

    backup folder

--- python/mail_automation.py
+++ python/mail_automation.py
@@ -8,7 +8,7 @@ def save_backup(body):
     # get the directory where the script is located
     script_dir = os.path.dirname(os.path.abspath(__file__))
     timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
-    file_path = os.path.join(script_dir, "../res/state backup " + timestamp + ".txt")
+    file_path = os.path.join(script_dir, "../res/backups/state backup " + timestamp + ".txt")
     with open(file_path, "w") as f:
         f.write(body)
 
