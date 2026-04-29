6722b78456406db68314574b01c2cf254c97fb06
Kasperls <kasperls004@gmail.com>
2026-04-29 11:07:09 +0200

    Fixing small errors

--- src/Group.h
+++ src/Group.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include <stdexcept>
 
 enum class Group{
     NONE, 
@@ -23,7 +24,7 @@ enum class Group{
 };
 
 inline std::string getTextFromGroup(Group group) {
-    static const std::unordered_map<std::string, Group> table = {
+    static const std::unordered_map<Group, std::string> table = {
         {Group::FG, "FG",    },
         {Group::ITK, "ITK",  },
         {Group::SM, "SM",    },
@@ -39,7 +40,7 @@ inline std::string getTextFromGroup(Group group) {
         {Group::KU, "KU",    },
         {Group::LOK, "LOK",  },
         {Group::KS, "KS",    },
-        {Group::SKP  "SKP",  }
+        {Group::SKP, "SKP",  }
     };
     auto it = table.find(group);
     return (it != table.end()) ? it->second : "NONE";
@@ -49,27 +50,27 @@ inline Group createGroupFromString(std::string str) {
     std::string upper = "";
     for (char& c : str) {
         if (!std::isalpha(c)) {
-            throw std::exception("INVALID GROUP NAME");
+            throw std::invalid_argument("INVALID GROUP NAME");
         }
         upper.push_back(std::toupper(c));
     }
 
     static const std::unordered_map<std::string, Group> table = {
-        {"FG",  Group::FG,  },
-        {"ITK", Group::ITK, },
-        {"SM",  Group::SM,  },
-        {"FK",  Group::FK,  },
-        {"REGI",Group::REGI,},
-        {"VK",  Group::VK,  },
-        {"DG",  Group::DG,  },
-        {"KSG", Group::KSG, },
-        {"ARK", Group::ARK, },
-        {"GU",  Group::GU,  },
-        {"SIT", Group::SIT, },
-        {"MG",  Group::MG,  },
-        {"KU",  Group::KU,  },
-        {"LOK", Group::LOK, },
-        {"KS",  Group::KS,  },
+        {"FG",  Group::FG   },
+        {"ITK", Group::ITK  },
+        {"SM",  Group::SM   },
+        {"FK",  Group::FK   },
+        {"REGI",Group::REGI },
+        {"VK",  Group::VK   },
+        {"DG",  Group::DG   },
+        {"KSG", Group::KSG  },
+        {"ARK", Group::ARK  },
+        {"GU",  Group::GU   },
+        {"SIT", Group::SIT  },
+        {"MG",  Group::MG   },
+        {"KU",  Group::KU   },
+        {"LOK", Group::LOK  },
+        {"KS",  Group::KS   },
         {"SKP", Group::SKP  }
     };
 
